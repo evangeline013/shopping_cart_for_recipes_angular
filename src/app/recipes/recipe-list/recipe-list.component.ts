0206a4b0b3903dcc878aa469bbe873c78968c792
Eva <evangeline013@gmail.com>
2019-06-23 14:07:03 -0400

    Pass recipe data from recipe-list to recipe-item component, fire event to inform recipe-detail of selected recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,13 +7,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Test Recipe', 'This is a test', 'https://www.wellplated.com/wp-content/uploads/2018/04/Mediterranean-Pasta-recipe-600x792.jpg'),
+    new Recipe('Another Recipe', 'This is a test', 'https://www.wellplated.com/wp-content/uploads/2018/04/Mediterranean-Pasta-recipe-600x792.jpg'),
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
