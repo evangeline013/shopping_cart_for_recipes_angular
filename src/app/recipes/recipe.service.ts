0b7f971955c57f6dc683aaaf5a89b3244b40ef02
Eva <evangeline013@gmail.com>
2019-09-06 16:42:49 -0400

    Add ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,41 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
-  
+
   private recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 'https://www.wellplated.com/wp-content/uploads/2018/04/Mediterranean-Pasta-recipe-600x792.jpg'),
-    new Recipe('Another Recipe', 'This is a test', 'https://www.wellplated.com/wp-content/uploads/2018/04/Mediterranean-Pasta-recipe-600x792.jpg'),
+    new Recipe(
+      'Schnitzel',
+      'A tasty Schnitzel',
+      'https://www.daringgourmet.com/wp-content/uploads/2014/03/Schnitzel-5.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Lemon', 1)
+      ]
+    ),
+    new Recipe(
+      'Fat Burger',
+      'A tasty and fat burger',
+      'https://natashaskitchen.com/wp-content/uploads/2019/04/Best-Burger-4.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1),
+        new Ingredient('Tomato', 1)
+      ]
+    ),
   ];
+
+  constructor(private shoppingListService: ShoppingListService) {}
 
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addMultipleIngredients(ingredients);
   }
 }
